0009b6061fac9909bffec9d99b0170df3e7fe5f7
quislingbomb <jasontomjiang@gmail.com>
2024-05-06 19:38:11 -0700

    Home Page Header

---
+++ client/src/Main.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import './Main.css';
+import logo from './assets/capy.png'
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <img src={logo} className="Main-logo" alt="logo"/>
+
+        <h1 className="Main-title">CAPY</h1>
+        <button type="button" className="Main-login" variant="contained">Login</button>
+        
+
+
+      </header>
+      <body className="Main-body">
+        Ok Pull up
+
+
+
+        
+
+      </body>
+
+
+
+      
+    </div>
+  );
+}
+
+export default Main;
