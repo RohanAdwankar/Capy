8a75eb38b134c02a8905e6f6e9972e7196590149
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-05-24 14:01:49 -0700

    Build Event component

--- src/pages/AllEvents.js
+++ src/pages/AllEvents.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import Event from "./Event";
 
 export default function AllEvents() {
 	return (
 		<div className="flex justify-center items-center h-full">
 			<div className="flex items-center my-5">
-				<p>This is where you will see all events.</p>
 			</div>
+			<Event/>
+			<Event/>
+			<Event/>
 		</div>
 	);
 }
