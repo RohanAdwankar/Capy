0519acc9c1f995fae0352b85f8bc15bdf39bc55c
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-06-01 16:39:59 -0700

    pull up song plays

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,13 @@
 import defEventPic from '../assets/defEvent.jpg';
 import React, { useState, useEffect } from 'react';
+import capy from '../assets/capy.png';
+import song from '../assets/songCrop.mp3';
 //import faCalendarDays from FontAwesomeIcon; 
 
 export default function Event({eventData}){
   const [likes, setLikes] = useState(0);
   const [showComments, setShowComments] = useState(false);
+  const [showAnimation, setShowAnimation] = useState(false);
 
   const handleCommentClick = () =>{
     setShowComments(!showComments);
@@ -13,6 +16,13 @@ export default function Event({eventData}){
   const handleLikeClick = () => {
     setLikes(likes+1);
   };
+
+  let audio = new Audio(song)
+
+  const start = () => {
+    audio.play()
+    console.log("playing")
+  }
   
   return (
     <div className="bg-white shadow-lg rounded-lg p-4">
@@ -21,6 +31,20 @@ export default function Event({eventData}){
           <button className="absolute top-0 right-0 bg-blue-500 text-white px-4 py-2 rounded">
             Pull Up
           </button>
+          <button
+            onClick={() => {
+              setShowAnimation(!showAnimation);
+              start();
+            }}
+            className="absolute top-0 right-0 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Pull Up
+          </button>
+          {/* <img
+            src={capy}
+            alt="Animation"
+            className={`object-scale-down h-10 w-10 absolute transform transition-transform duration-500 ease-in-out ${showAnimation ? 'translate-x-full' : ''}`}
+          /> */}
           <h1 className="text-2xl font-bold">{eventData.title}</h1>
           <h2>{eventData.user}</h2>
           <p>{eventData.location}, {eventData.date}</p>
