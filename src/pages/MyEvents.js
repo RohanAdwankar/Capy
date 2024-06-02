01aa2bc8a7b81e4a0051b6fa909b1fd01b9cd242
benman604 <57852213+benman604@users.noreply.github.com>
2024-06-01 23:01:15 -0700

    ui formatting

--- src/pages/MyEvents.js
+++ src/pages/MyEvents.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 
 export default function MyEvents() {
-	return (
-		<div className="">
+	const [type, setType] = useState("RSVP'd");
+	const [filter, setFilter] = useState('');
 
-			<div className="flex items-center">
-				<p>Create an event or click "Pull Up" to an event to see it show up here.</p>
+	return (
+		<div className="block my-5">
+			<div className="flex justify-center mb-5">
+				<div className="mr-20">
+					<select className="rounded bg-gray-100 p-2 pl-5 mb-2 w-full h-full" onChange={(e)=>{setType(e.target.value)}}>
+						<option value="RSVP'd">RSVP'd Events</option>
+						<option value="your">Created Events</option>
+					</select>
+				</div>
+				<div className="flex">
+					<input type="text" placeholder={`Search ${type} events`} className="rounded bg-gray-100 p-2 pl-5 mb-2 w-full h-full"
+						onChange={(e) => {setFilter(e.target.value)}}
+					/>
+					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+						Search
+					</button>
+				</div>
 			</div>
 		</div>
 	);
