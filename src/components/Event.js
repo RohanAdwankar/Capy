330ae0051d3948ad4447baf739705fb8a5bd4e5e
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-06-02 15:24:51 -0700

    cleaned up but image isnt in database yet

--- src/components/Event.js
+++ src/components/Event.js
@@ -40,14 +40,6 @@ export default function Event({eventData, userID}){
           <div className="bg-white p-5 rounded shadow-lg w-3/4 h-3/4 overflow-auto relative">
             <button onClick={() => setIsModalOpen(false)} className="absolute top-2 right-2 bg-red-500 text-white px-4 py-2 rounded">Close</button>
             <h1 className="text-4xl mb-4">{eventData.title}</h1>
-            <div className="flex items-center">
-              <FontAwesomeIcon icon={faLocationDot}/>
-              <p className="ml-2">{eventData.location}</p>
-            </div>
-            <div className="flex items-center mt-2">
-              <FontAwesomeIcon icon={faCalendarDays} />
-              <p className="ml-2">{eventData.date}</p>
-            </div>
             <div className="relative flex items-center mb-4">
               <p> <FontAwesomeIcon icon={faLocationDot}/> {eventData.location}</p>
               <p>&nbsp;&nbsp;</p>
@@ -55,8 +47,6 @@ export default function Event({eventData, userID}){
               {/* <img src={user.profilePicture} alt="User" className="w-10 h-10 rounded-full mr-4" />
               <h2 className="text-xl">{user.name}</h2> */}
             </div>
-            <p className="mb-4">{eventData.location}</p>
-            <p className="mb-4">{eventData.date}</p>
             {/* <img src={eventData.picture} alt="Event" className="mb-4" /> */}
             <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4">Pull Up</button>
             <p className="mb-4">{eventData.description}</p>
