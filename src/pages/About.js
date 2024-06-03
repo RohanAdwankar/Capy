38c8ecfe7a14ffedc6aaf77d5a36150c17205270
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2024-06-02 21:01:22 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,34 +2,34 @@ import {React} from 'react';
 
 export default function About() {
     return (
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col md:flex-row bg-gray-100 p-8">
           <div className="md:w-2/3 text-black p-8 space-y-6">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-bold text-blue-600">
                 Connecting the campus, one "pull up" at a time!
             </h1>
-            <p>
+            <p className="text-lg text-gray-700">
                 Our mission is to reduce loneliness at UCLA, and connect the campus one "pull up" at a time!
             </p>
-            <p>
+            <p className="text-lg text-gray-700">
                 We are a team of UCLA students that are working on developing this not-for-profit project
             </p>
-            <p>
+            <p className="text-lg text-gray-700">
                 Here's what you can do using Capy!
             </p>
-            <ul class="list-disc pl-5 space-y-2">
-              <li class="text-sm text-gray-700"> 
+            <ul className="list-disc pl-5 space-y-2 text-gray-600">
+              <li className="text-sm"> 
                 Connect with your classmates by "pulling up" to fun events in the area!
               </li>
-              <li class="text-sm text-gray-700">
+              <li className="text-sm">
                 Make friends and build your own community by sending friend requests to other users you meet!
               </li>
-              <li class="text-sm text-gray-700">
+              <li className="text-sm">
                 Create new events and get the community involved with fun activities you're planning!
               </li>
             </ul>
           </div>
           <div className="md:w-1/3 flex items-center justify-center p-8">
-            <img src="../assets/coda.png" alt="Codas" className="rounded-full w-full" style={{ maxWidth: '600px' }}/>
+            <img src="../assets/coda.png" alt="Codas" className="rounded-full w-full shadow-lg" style={{ maxWidth: '600px' }}/>
           </div>
         </div>
       );
